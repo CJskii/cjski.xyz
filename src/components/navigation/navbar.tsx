dec3522c9d09d200ea1dbabf85fdca110f48cd29
CJskii <czareczarek3@gmail.com>
2024-09-22 11:51:01 +0100

    chore: update footer and navbar

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -9,83 +9,31 @@ import { HeaderSheet } from "./header-sheet";
 import { ConnectWalletButton } from "@/components/ui/connect-button";
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  ListItem,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { Typography } from "../ui/typography";
-import { Separator } from "@radix-ui/react-separator";
 import React from "react";
-
-interface DropdownLinks {
-  label: string;
-  paths: {
-    href: string;
-    label: string;
-    description: string;
-  }[];
-}
 
 interface NavLink {
   label: string;
   href: string;
 }
 
-const productLinks: DropdownLinks[] = [
+const linkData: NavLink[] = [
   {
-    label: "Products",
-    paths: [
-      {
-        href: "/product1",
-        label: "Product 1",
-        description: "Learn more about our first product offering.",
-      },
-      {
-        href: "/product2",
-        label: "Product 2",
-        description: "Explore the features of our second product.",
-      },
-      {
-        href: "/product3",
-        label: "Product 3",
-        description: "Get started with our third innovative product.",
-      },
-    ],
+    label: "Projects",
+    href: "/projects",
   },
   {
-    label: "Solutions",
-    paths: [
-      {
-        href: "/solution1",
-        label: "Solution 1",
-        description: "Discover our first solution for seamless operations.",
-      },
-      {
-        href: "/solution2",
-        label: "Solution 2",
-        description: "Unlock the power of our second seamless solution.",
-      },
-      {
-        href: "/solution3",
-        label: "Solution 3",
-        description: "Efficiently bridge the gap with our third solution.",
-      },
-    ],
-  },
-];
-
-const otherLinks: NavLink[] = [
-  {
-    label: "Documentation",
-    href: "/docs",
+    label: "About Me",
+    href: "/about",
   },
   {
-    label: "Examples",
-    href: "/example",
+    label: "Contact",
+    href: "/contact",
   },
 ];
 
@@ -113,8 +61,8 @@ export const Navbar: React.FC = () => {
             alt="light mode logo"
             className="hidden w-40 dark:block"
           /> */}
-          <Typography variant="h3" className="font-bold">
-            Logo Placeholder
+          <Typography variant="h4" className="font-bold tracking-widest">
+            cjski.xyz
           </Typography>
         </Link>
       </div>
@@ -135,8 +83,8 @@ export const Navbar: React.FC = () => {
           width={30}
           height={30}
         /> */}
-        <Typography variant="h3" className="font-bold">
-          Logo Placeholder
+        <Typography variant="h4" className="font-bold">
+          cjski.xyz
         </Typography>
       </Link>
       <div className="flex-1 justify-center hidden items-center gap-3 lg:flex">
@@ -159,36 +107,7 @@ export const NaviLinks: React.FC = () => {
   return (
     <NavigationMenu>
       <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="">Products</NavigationMenuTrigger>
-          <NavigationMenuContent className="flex">
-            {productLinks.map((navLink, index) => (
-              <ul
-                key={index}
-                className="flex flex-col w-[200px] gap-3 p-4 md:w-[250px] md:grid-cols-2 lg:w-[300px] "
-              >
-                <Typography className="" variant={"large"}>
-                  {navLink.label}
-                </Typography>
-                {navLink.paths.map((path, index) => (
-                  <React.Fragment key={path.label}>
-                    <ListItem title={path.label} href={path.href} className="">
-                      {path.description}
-                    </ListItem>
-                    {index !== navLink.paths.length - 1 && (
-                      <Separator
-                        orientation="horizontal"
-                        className="dark:border-[#E8E8E8]/20 border-[1px] border-black/20"
-                      />
-                    )}
-                  </React.Fragment>
-                ))}
-              </ul>
-            ))}
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-
-        {otherLinks.map((navLink, index) => (
+        {linkData.map((navLink, index) => (
           <NavigationMenuItem key={index}>
             <Link href={navLink.href} legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
@@ -205,25 +124,11 @@ export const NaviLinks: React.FC = () => {
 export const MobileNavLinks: React.FC = () => {
   return (
     <>
-      {otherLinks.map((navLink, index) => (
+      {linkData.map((navLink, index) => (
         <React.Fragment key={index}>
           <Link href={navLink.href} passHref>
             <Button className="w-full">{navLink.label}</Button>
           </Link>
-        </React.Fragment>
-      ))}
-      {productLinks.map((navLink, index) => (
-        <React.Fragment key={index}>
-          <Typography key={index} variant={"large"}>
-            {navLink.label}
-          </Typography>
-          {navLink.paths.map((path, index) => (
-            <React.Fragment key={index}>
-              <Link href={path.href} passHref>
-                <Button className="w-full">{path.label}</Button>
-              </Link>
-            </React.Fragment>
-          ))}
         </React.Fragment>
       ))}
     </>
