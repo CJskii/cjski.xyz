11eee44816716f5f97292e4b11afcb1e9e8af167
CJskii <czareczarek3@gmail.com>
2024-09-24 12:21:23 +0100

    chore: change network order

---
+++ src/components/contracts/network-icons.tsx
@@ -0,0 +1,32 @@
+import { networks } from "@/constants/contracts";
+import AvatarCircles from "../magicui/avatar-circles";
+
+interface NetworkIconsProps {
+  display: string;
+  numNetworks?: number;
+}
+
+export const NetworkIcons: React.FC<NetworkIconsProps> = ({
+  display,
+  numNetworks = 0,
+}) => {
+  const networkIcons = networks.map((network) => {
+    return network.iconUrl;
+  });
+
+  if (display === "all") {
+    return <AvatarCircles avatarUrls={networkIcons} numPeople={numNetworks} />;
+  }
+
+  const displayNumber = parseInt(display);
+  if (!isNaN(displayNumber)) {
+    return (
+      <AvatarCircles
+        avatarUrls={networkIcons.slice(0, displayNumber)}
+        numPeople={networkIcons.length - displayNumber}
+      />
+    );
+  }
+
+  return <AvatarCircles avatarUrls={networkIcons} numPeople={26} />;
+};
