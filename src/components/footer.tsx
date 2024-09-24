bf1a02b84d3e245ba2159fcbb217a66cef29eb4a
CJskii <czareczarek3@gmail.com>
2024-09-24 14:07:02 +0100

    feat: add farcaster icon

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
-import { GitHubIcon, TelegramIcon, XIcon } from "@/assets/icons/social";
+import {
+  FarcasterIcon,
+  GitHubIcon,
+  TelegramIcon,
+  XIcon,
+} from "@/assets/icons/social";
+import { Button } from "./ui/button";
 
 interface FooterData {
   label: string;
@@ -9,6 +15,16 @@ interface FooterData {
 
 const socialLinks: FooterData[] = [
   {
+    label: "Warpcast",
+    href: "https://warpcast.com/cjski",
+    children: <FarcasterIcon />,
+  },
+  {
+    label: "Twitter/X",
+    href: "https://twitter.com/cjski_web3",
+    children: <XIcon />,
+  },
+  {
     label: "GitHub",
     href: "https://github.com/CJskii",
     children: <GitHubIcon />,
@@ -17,11 +33,6 @@ const socialLinks: FooterData[] = [
     label: "Telegram",
     href: "https://t.me/cj2077",
     children: <TelegramIcon />,
-  },
-  {
-    label: "Twitter/X",
-    href: "https://twitter.com/cjski_web3",
-    children: <XIcon />,
   },
 ];
 
@@ -60,15 +71,17 @@ const FooterLink: React.FC<FooterData> = ({ href, children }) => {
 
 const FooterIconLink: React.FC<FooterData> = ({ href, label, children }) => {
   return (
-    <Link
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label={label}
-      className="text-gray-500 hover:text-gray-900 transition"
-    >
-      {children}
-    </Link>
+    <Button variant={"ghost"} size={"icon"}>
+      <Link
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label={label}
+        className="text-muted-foreground"
+      >
+        {children}
+      </Link>
+    </Button>
   );
 };
 
@@ -76,7 +89,7 @@ const FooterContainer: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <footer className="bg-white/50 dark:bg-black/50 rounded-t-xl px-6 pb-10 pt-20 md:p-16 md:py-8 flex flex-col lg:flex-row items-start justify-between gap-4">
+    <footer className="bg-white/50 dark:bg-black/50 rounded-t-xl px-6 pb-10 pt-12 md:p-16 md:py-8 flex flex-col lg:flex-row items-start justify-between gap-4">
       {children}
     </footer>
   );
